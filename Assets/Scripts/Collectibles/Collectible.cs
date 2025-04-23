e8ae7e930cc3a4fea110a404c54b10a4325e43fb
Ilechko Serhii <otakuvibri@gmail.com>
2025-04-23 19:43:00 +0100

    UI Ready

---
+++ Assets/Scripts/Collectibles/Collectible.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    // Событие, которое будет вызываться при подборе
+    public static event System.Action<int> OnCollectiblePicked;
+
+    [SerializeField] private int value = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OnCollectiblePicked?.Invoke(value);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Collectibles/CollectibleManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CollectibleManager : MonoBehaviour
+{
+    public static CollectibleManager Instance;
+
+    private int totalCollected = 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        Collectible.OnCollectiblePicked += AddCollectible;
+    }
+
+    private void OnDisable()
+    {
+        Collectible.OnCollectiblePicked -= AddCollectible;
+    }
+
+    private void AddCollectible(int amount)
+    {
+        totalCollected += amount;
+        Debug.Log("Collected: " + totalCollected);
+        // Здесь можно добавить обновление UI, если оно уже есть
+    }
+
+    public int GetCollectedCount()
+    {
+        return totalCollected;
+    }
+}
